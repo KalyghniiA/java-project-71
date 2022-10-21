3672408d8db95613ae6184c790e0a04cc69f36aa
Александр <kalyghnii@gmail.com>
2022-10-21 15:51:01 +0500

    fixed data

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -47,7 +47,6 @@ public class Differ {
     }
 
     private static String formatResult(String format,  Map<String, StatusDataElement> resultDiff) throws IOException {
-        System.out.println(format);
         switch (format.toLowerCase()) {
             case "json":
                 return createDifferToJSON(resultDiff);
